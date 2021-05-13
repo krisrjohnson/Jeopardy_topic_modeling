81ca09a4997ca088aba5fde8895913074e1f4024
pnavo <philnavo@gmail.com>
2021-05-12 18:47:56 -0700

    added some functions and preload json file as "df"

--- jeopardy_funcs.py
+++ jeopardy_funcs.py
@@ -6,9 +6,19 @@ import numpy as np
 import pandas as pd
 import json
 import matplotlib.pyplot as plt
+import nltk
+import re
+
+from nltk.corpus import stopwords
+from gensim.models import Word2Vec
+from gensim.similarities.annoy import AnnoyIndexer
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.decomposition import NMF, LatentDirichletAllocation 
+
+
+# preload dataframe
+df = pd.read_json("JEOPARDY_QUESTIONS1.json")
 
 def get_topics(df, num_topics, rd:str=None, max_df:float=0.95):
 	"""
@@ -68,6 +78,35 @@ def get_difficulty(df, rd: str):
 	Get difficulty for each value group.
 	"""
 	pass
+
+def w2v(df):
+    categories = set([i.lower() for i in df['category']])
+    categories = list(categories)
+    categories = [re.sub('[^a-zA-Z]', ' ', cat) for cat in categories]
+    categories = [re.sub(r'\s+', ' ', cat) for cat in categories]
+    categories = [c.strip() for c in categories]
+    category = [nltk.sent_tokenize(c) for c in categories]
+    all_words = [nltk.word_tokenize(c) for cat in category for c in cat]
+    for i in range(len(all_words)):
+        all_words[i] = [w for w in all_words[i] if w not in stopwords.words('english')]
+    word2vec = Word2Vec(all_words, min_count=2)
+    vocabulary = word2vec.wv
+    return word2vec, vocabulary
+
+def summary(x:str, word2vec, vocabulary):
+    annoy_index = AnnoyIndexer(word2vec, 100)
+    vector = vocabulary[x]
+    # The instance of AnnoyIndexer we just created is passed
+    approximate_neighbors = vocabulary.most_similar([vector], topn=11, indexer=annoy_index)
+    # Neatly print the approximate_neighbors and their corresponding cosine similarity values
+    print("Approximate Neighbors")
+    for neighbor in approximate_neighbors:
+        print(neighbor)
+    normal_neighbors = vocabulary.most_similar([vector], topn=11)
+    print("\nExact Neighbors")
+    for neighbor in normal_neighbors:
+        print(neighbor)
+
 
 # for debugging
 if __name__ == '__main__':

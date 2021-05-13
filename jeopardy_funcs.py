5860e426cc5b564614acdb246f2bc6cce0ac3961
Kris <johnson.krj@gmail.com>
2021-05-13 02:50:07 -0400

    Added get_topic_word_counts, returns a dict with keys of topic_id and values of a Counter() with words and their counts in that topic. get_topic_word_counts has had wildly differing runtimes, and is certainly not efficient.

--- jeopardy_funcs.py
+++ jeopardy_funcs.py
@@ -8,6 +8,8 @@ import json
 import matplotlib.pyplot as plt
 import nltk
 import re
+import time
+from collections import Counter, defaultdict
 
 from nltk.corpus import stopwords
 from gensim.models import Word2Vec
@@ -20,76 +22,82 @@ from sklearn.decomposition import NMF, LatentDirichletAllocation
 # preload dataframe
 df = pd.read_json("JEOPARDY_QUESTIONS1.json")
 
-def get_topics(df, num_topics, rd:str=None, max_df:float=0.95, min_df:int=3):
-	"""
-	Finds all topics for the 'question'
-	column of data for the specified round.
-	Arguments:
-		- df: DataFrame of jeopardy data
-		- rd: Specifies the round to subset
-				 on. Should be one of 'Jeopardy!',
-				 'Double Jeopardy!',
-				 'Final Jeopardy!' or None. If
-				 None, use entire data
-		- max_data: Passed as 'max_data' in the 
-					 TfidataVectorizer. Specifies a
-					 cutoff point to exclude terms
-					 with too high document
-					 frequency.
+def get_topics(df, num_topics, rd:str=None, max_df:float=0.95, min_df:int=3, ret_tfidf=False):
+  """
+  Finds all topics for the 'question'
+  column of data for the specified round.
+  Arguments:
+    - df: DataFrame of jeopardy data
+    - rd: Specifies the round to subset
+         on. Should be one of 'Jeopardy!',
+         'Double Jeopardy!',
+         'Final Jeopardy!' or None. If
+         None, use entire data
+    - max_data: Passed as 'max_data' in the 
+           TfidataVectorizer. Specifies a
+           cutoff point to exclude terms
+           with too high document
+           frequency.
      - min_df: TfidataVectorizer min freq count
 
-	Returns:
-		W: np.array, the document-topic matrix
-		H: np.array, the topic-term matrix
-		data: pd.DataFrame, df with new column 'topic'
-			 specifying the topic for the question
-		vocab: dict, word to index from TFIDF
-	"""
-	if rd is not None:
-		data = df.loc[df['round'] == rd]
-	else:
-		data = df.copy()
+  Returns:
+    W: np.array, the document-topic matrix
+    H: np.array, the topic-term matrix
+    data: pd.DataFrame, df with new column 'topic'
+       specifying the topic for the question
+    vocab: dict, word to index from TFIDF
+  """
+  if rd is not None:
+    data = df.loc[df['round'] == rd]
+  else:
+    data = df.copy()
 
-	# tfidf
-	vectorizer = TfidfVectorizer(
-		strip_accents='ascii',
-		stop_words='english',
-		max_df=max_df,
+  # tfidf
+  vectorizer = TfidfVectorizer(
+    strip_accents='ascii',
+    stop_words='english',
+    max_df=max_df,
     min_df=min_df
-	)
-	tfidf_matrix = vectorizer.fit_transform(data['question'])
+  )
+  tfidf_matrix = vectorizer.fit_transform(data['question'])
 
-	# NMF
-	model = NMF(n_components=num_topics, init='random', max_iter=400)
-	W = model.fit_transform(tfidf_matrix)
-	H = model.components_
+  # NMF
+  model = NMF(n_components=num_topics, init='random', max_iter=400)
+  W = model.fit_transform(tfidf_matrix)
+  H = model.components_
 
-	topics = np.argmax(W, axis=1)
-	data.loc[:, 'topic'] = topics
+  topics = np.argmax(W, axis=1)
+  data.loc[:, 'topic'] = topics
 
-	return W, H, data, vectorizer.vocabulary_
+  if ret_tfidf:
+    return W, H, data, vectorizer.vocabulary_, tfidf_matrix
+
+  return W, H, data, vectorizer.vocabulary_
+
 
 def get_difficulty(df):
-	"""
-	Get difficulty for each value group.
-	Arguments:
-		- df: jeopardy question data
-	"""
-	data = df.copy()
-	data.loc[:, 'difficulty'] = data.groupby(['show_number']).rank(method='dense')['value']
-	return data
+  """
+  Get difficulty for each value group.
+  Arguments:
+    - df: jeopardy question data
+  """
+  data = df.copy()
+  data.loc[:, 'difficulty'] = data.groupby(['show_number']).rank(method='dense')['value']
+  return data
+
 
 def prep_data(df):
-	"Function to prep data"
-	data = df.copy()
+  "Function to prep data"
+  data = df.copy()
 
-	data.fillna(0)  # for missing question values
-	# convert value from $500 -> 500.0
-	data.loc[:, 'value'] = data['value'].str[1:].str.replace(',','').astype(np.float32)
-	data.loc[:, 'air_date'] = pd.to_datetime(data.loc[:, 'air_date'])
-	data.loc[:, 'year'] = data.loc[:, 'air_date'].dt.year
+  data.fillna(0)  # for missing question values
+  # convert value from $500 -> 500.0
+  data.loc[:, 'value'] = data['value'].str[1:].str.replace(',','').astype(np.float32)
+  data.loc[:, 'air_date'] = pd.to_datetime(data.loc[:, 'air_date'])
+  data.loc[:, 'year'] = data.loc[:, 'air_date'].dt.year
 
-	return data
+  return data
+
 
 def w2v(df):
     categories = set([i.lower() for i in df['category']])
@@ -104,6 +112,7 @@ def w2v(df):
     word2vec = Word2Vec(all_words, min_count=2)
     vocabulary = word2vec.wv
     return word2vec, vocabulary
+
 
 def summary(x:str, word2vec, vocabulary):
     annoy_index = AnnoyIndexer(word2vec, 100)
@@ -120,11 +129,39 @@ def summary(x:str, word2vec, vocabulary):
         print(neighbor)
 
 
+def get_topic_word_counts(df, vocab, tfidf_matrix, verbose=False):
+    '''
+    # Given original dataframe with topics column, vocab, and sparse matrix
+    # where rows of sparse_matrix align with rows of dataframe
+    # create defaultdict whose keys are the topics
+    # and values are words with counts
+    '''
+    idx2word = {idx: word for word, idx in vocab.items()}
+    vf = np.vectorize((lambda x: idx2word[x]))
+
+    start = time.time()
+    topic_counts = defaultdict(Counter)
+
+    checkpoints = [20_000 * _ for _ in range(tfidf_matrix.shape[0])]
+
+    for i in range(tfidf_matrix.shape[0]):
+        _, cols = tfidf_matrix[i, :].nonzero()
+        if len(cols) == 0:
+            continue
+        topic_counts[df.loc[i].topic].update(set(vf(cols)))
+        #     print(set(vf(cols)))
+        if verbose:
+            if i in checkpoints:
+                print(f'{i:,}, {time.time() - start}')
+    
+    return topic_counts
+
+
 # for debugging
 if __name__ == '__main__':
-	# df = pd.read_json('JEOPARDY_QUESTIONS1.json')
-	# get_topics(df, 25)
-	pass
+  # df = pd.read_json('JEOPARDY_QUESTIONS1.json')
+  # get_topics(df, 25)
+  pass
 
 
 
